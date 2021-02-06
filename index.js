b98663a179f14a29fa9475c5ea003dfffa205b21
cory neel <corycneel@gmail.com>
2021-02-06 17:20:07 -0600

    fixed conflicting js i created on two separate days

--- index.js
+++ index.js
@@ -82,8 +82,10 @@ async function init() {
         
         // Write README to results directory
         await writeFileAsync('./result/README.md', generateContent);
+        console.log('✔️  Successfully wrote to README.md');
     }   
     catch(err) {
+        console.log(err);
     }
   }
   
