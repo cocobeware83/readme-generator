7469486955639dcc8d4f3008dc6ae8e4cda93780
cory neel <corycneel@gmail.com>
2021-02-06 02:03:07 -0600

    added validation checks to question prompts

--- Develop/index.js
+++ Develop/index.js
@@ -13,53 +13,50 @@ function promptUser(){
             type: "input",
             name: "title",
             message: "What is this project's title?",
-        },
+            // validate that an answer has been provided
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
         {
             type: "input",
             name: "description",
-            message: "Please provide a brief description of your project: "
-        },
+            message: "Please provide a brief description of your project: ",
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
         {
             type: "input",
             name: "installation",
             message: "Please describe the installation process: ",
-        },
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
         {
             type: "input",
             name: "usage",
-            message: "What is this project used for?"
-        },
+            message: "What is this project used for?",
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
         {
             type: "list",
             name: "license",
             message: "Please choose the license that is applicable for this project: ",
-            choices: [
-                "Apache",
-                "ISC",
-                "MIT",
-                "Mozilla",
-                "GNU",
-            ]
-        },
+            choices: ["Apache","ISC","MIT","Mozilla","GNU"],
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
         {
             type: "input",
             name: "contributing",
-            message: "Who contributed to this project?"
-        },
+            message: "Who contributed to this project?",
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
         {
             type: "input",
             name: "tests",
-            message: "Is there a test included?"
-        },
+            message: "Is there a test included?",
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
         {
             type: "input",
             name: "username",
-            message: "Please enter your GitHub username: "
-        },
+            message: "Please enter your GitHub username: ",
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
         {
             type: "input",
             name: "email",
-            message: "Please enter your email address: "
+            message: "Please enter your email address: ",
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
+
         }
     ]);
 } 
