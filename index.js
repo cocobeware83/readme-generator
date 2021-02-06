4479a4160b199f719334b0ab9afd536320019684
cory neel <corycneel@gmail.com>
2021-02-06 16:29:56 -0600

    fixed issue with improper syntax

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Required Packages/Dependencies
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
+const utils= require('utils');
 const generateMarkdown = require("./utils/generateMarkdown")
-const writeFileAsync = util.promisify(fs.writeFile);
+const writeFileAsync = utils.promisify(fs.writeFile);
 
 // Write Array of Questions
 
@@ -13,51 +13,63 @@ function promptUser(){
             type: "input",
             name: "title",
             message: "What is this project's title?",
-            // validate that an answer has been provided
-            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}
+        }},
         {
             type: "input",
             name: "description",
             message: "Please provide a brief description of your project: ",
-            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}
+        }},
         {
             type: "input",
             name: "installation",
             message: "Please describe the installation process: ",
-            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}
+        }},
         {
             type: "input",
             name: "usage",
             message: "What is this project used for?",
-            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}
+        }},        
         {
             type: "list",
             name: "license",
             message: "Please choose the license that is applicable for this project: ",
             choices: ["Apache","ISC","MIT","Mozilla","GNU"],
-            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}
+        }},        
         {
             type: "input",
-            name: "contributing",
+            name: "contribution",
             message: "Who contributed to this project?",
-            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}
+        }},        
         {
             type: "input",
             name: "tests",
             message: "Is there a test included?",
-            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}
+        }},        
+        {
+            type: "input",
+            name: "questions",
+            message: "Who do I contact if I have any questions?",
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}
+        }},    
         {
             type: "input",
             name: "username",
             message: "Please enter your GitHub username: ",
-            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}
+        }},        
         {
             type: "input",
             name: "email",
             message: "Please enter your email address: ",
-            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}},
-
-        }
+            validate: (value)=>{if(value){return true} else {return 'please provide a valid response to continue'}
+        }},
     ]);
 } 
 
@@ -68,7 +80,7 @@ async function init() {
         const data = await promptUser();
         const generateContent = generateMarkdown(data);
         
-        // Write README
+        // Write README to results directory
         await writeFileAsync('./result/README.md', generateContent);
     }   
     catch(err) {
